b8508f1396aa44f3c3ed82bc32ddc18bf1e09899
imyakin <myakinalex@yandex.ru>
2023-09-22 12:57:42 +0400

    Revert "git hooks added"
    This reverts commit 5ceb28a6e35b99cc92a7b28ca06119528d9aa136.

--- app/contacts/sendEmail.ts
+++ app/contacts/sendEmail.ts
@@ -4,7 +4,7 @@ import { FormikValues } from "formik";
 import { RESEND_API_KEY } from "@/app/contacts/constants";
 
 const resend = new Resend(RESEND_API_KEY);
-console.log("hello world ddsc");
+console.log("hello world dds");
 export const sendMail = async (values: FormikValues) => {
   await resend.emails.send({
     from: "PersonalWebSite <onboarding@resend.dev>",
