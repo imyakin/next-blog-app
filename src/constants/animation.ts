f295bfd6aa74bdb55e5ac1d1f7f9021dd98d6ab1
imyakin <myakinalex@yandex.ru>
2023-09-25 14:47:48 +0400

    fix types for animation.ts

--- src/constants/animation.ts
+++ src/constants/animation.ts
@@ -3,7 +3,7 @@ export const animation  = {
     x: -30,
     opacity: 0
   },
-  visible: custom => ({
+  visible: (custom: number) => ({
     x: 0,
     opacity: 1,
     transition: { delay: custom * 0.2, duration: 1 }
